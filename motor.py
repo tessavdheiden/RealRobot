352a1fa42b5c2c77ea9b6c06463a10e9ae22b828
CGBurger <constantin-berger@live.de>
2019-09-30 09:15:18 +0200

    Path-Tracking: Reaching points behind Robot

--- motor.py
+++ motor.py
@@ -1,11 +1,14 @@
 import math
 import time
 from gps import GPS
+from gpiozero import Motor
 
 v_m = 0.1  # average-speed in m/s +++ for this first attempt the average-speed is a constant (it will depend on action)
-b = 0.22  # wheel distance in m +++ needs to be adjusted
 FPS = 5  # ???
 robot_gps = GPS('robot')
+
+#  motor1 = Motor(23, 24)
+#  motor2 = Motor(27, 17)
 
 
 def get_odo_x():
@@ -51,24 +54,41 @@ def get_odo_theta():  # pending .......
 
 
 def pure_pursuit(x_new, y_new):
-    current_x = get_odo_x()
-    current_y = get_odo_y()
-    current_theta = get_odo_theta()
+    y_1 = -math.sin(get_odo_theta()) * x_new + math.cos(get_odo_theta()) * y_new
+    if y_1 >= 0:
+        goal_behind = 0
+    elif y_1 < 0:
+        goal_behind = 1
 
-    dx = x_new - current_x
-    dy = y_new - current_y
-    dl = math.sqrt(dy * dy + dx * dx)
-    alpha = math.atan2(dy, dx) + current_theta
-    kappa = (2 * math.sin(alpha)) / dl
+    if goal_behind == 1:
+        if (math.cos(get_odo_theta()) * x_new + math.sin(get_odo_theta()) * y_new) > 0:
+            kappa = -1
+        elif (math.cos(get_odo_theta()) * x_new + math.sin(get_odo_theta()) * y_new) <= 0:
+            kappa = -2
+    elif goal_behind == 0:
+        current_x = get_odo_x()
+        current_y = get_odo_y()
+        current_theta = get_odo_theta()
+
+        dx = x_new - current_x
+        dy = y_new - current_y
+        dl = math.sqrt(dy * dy + dx * dx)
+        alpha = math.atan2(dy, dx) + current_theta
+        kappa = (2 * math.sin(alpha)) / dl
     return kappa
 
 
 def kappa_to_v(kappa):
     b = 0.22  # wheel distance in m +++ needs to be adjusted
-    print("converting kappa to v")
-    v_l = v_m * (1 - kappa * b)
-    v_r = v_m * (1 + kappa * b)
-    v = [v_l, v_r]
+    if kappa == -1:
+        v = [v_m, -v_m]
+    elif kappa == -2:
+        v = [-v_m, v_m]
+    elif kappa >= 0:
+        print("converting kappa to v")
+        v_l = v_m * (1 - kappa * b)
+        v_r = v_m * (1 + kappa * b)
+        v = [v_l, v_r]
     return v
 
 
@@ -77,3 +97,5 @@ def set_speed(v):  # pending .......
     factor = 1
     speed_left = factor*v[0]
     speed_right = factor*v[1]
+    #  motor1.forward(speed_left)
+    #  motor2.forward(speed_right)

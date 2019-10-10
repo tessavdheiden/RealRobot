5c4c56f057424d2eca2c248e718e4c5f4d3847c0
Tessa van der Heiden <tessavdheiden@gmail.com>
2019-10-10 14:38:40 +0200

    Testaction sent to motor can be executed

--- main_raspberry.py
+++ main_raspberry.py
@@ -5,36 +5,40 @@ import support.util as util
 from model.episode import Episode
 from support.gps_drive import Go, nn_drive
 
-ACTION_FPS = 2
+ACTION_FPS = 0.5
 
 def main():
     db = fbdb.db
     episode = Episode()
     while True:
-        while episode.episode_started:
+        while True:
             actionDict = db.child('currentEpisode').child('action').get().val()
-            (vx, vy) = (actionDict['vx'], actionDict['vy'])
+            (vx, vy) = (actionDict['vx'], actionDict['vy']) or (0.5, 0.5)
 
-            if vx < vy:
-                motorLeft = .5
-                motorRight = .75
-                mode = 1
-            elif vx > vy:
-                motorLeft = .75
-                motorRight = .5
-                mode = 2
-            elif vx == vy and vx > 0:
-                motorLeft = .75
-                motorRight = .5
-                mode = 1
-            elif vx == vy and vx < 0:
-                motorLeft = .75
-                motorRight = .5
-                mode = 2
-            else:
-                motorLeft = .0
-                motorRight = .0
-                mode = 0
+
+            motorLeft = vx
+            motorRight = vy
+            mode = 1
+            # if vx < vy:
+            #     motorLeft = .5
+            #     motorRight = .75
+            #     mode = 1
+            # elif vx > vy:
+            #     motorLeft = .75
+            #     motorRight = .5
+            #     mode = 2
+            # elif vx == vy and vx > 0:
+            #     motorLeft = .75
+            #     motorRight = .5
+            #     mode = 1
+            # elif vx == vy and vx < 0:
+            #     motorLeft = .75
+            #     motorRight = .5
+            #     mode = 2
+            # else:
+            #     motorLeft = .0
+            #     motorRight = .0
+            #     mode = 0
 
             print("vx = {}, vy = {}".format(vx, vy))
             nn_drive(motorLeft, motorRight, mode)
